c50d3c0e337de112550a66785ceb4a62779ae2d3
Vegard Løkås <vegardlokas@hotmail.com>
2024-11-26 22:15:14 +0100

    day1

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,1 +1,5 @@
-# Write your code below this line 👇
+print("Welcome to this simple program")
+town = input("What city did you grow up in?")
+pet = input("Name of a pet?")
+
+print("your cool band name is: " + town + " " + pet)
